3e6c87f303b8c642d5b9019804678e8122df5561
Dmytro Popov <thedmitryp@ukr.net>
2024-03-21 22:58:33 +0000

    Created game constants

---
+++ checkers_front/lib/src/domain/constants.dart
@@ -0,0 +1,8 @@
+/// A size of the game board (in cells).
+const boardSide = 8;
+
+/// The amount of cells the board contains.
+const boardCellCount = boardSide * boardSide;
+
+/// The last index in a row or column.
+const lastIndex = boardSide - 1;
