f9e3eb27575f11ef8688128582b05538dbe56aa2
Dmytro Popov <thedmitryp@ukr.net>
2024-03-21 22:21:47 +0000

    Added initial Flutter project structure

---
+++ checkers_front/lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'presentation/main_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
---
+++ checkers_front/lib/src/presentation/main_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Placeholder(
+          child: Center(
+            child: Text(
+              'Placeholder',
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
