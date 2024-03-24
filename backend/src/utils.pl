ba6c896eddbb86ff76dc83f981db4b242e227979
Kathryn Verkhohliad <verkhogliadkate@gmail.com>
2024-03-24 18:48:01 +0100

    implemented printing board into console

--- backend/src/utils.pl
+++ backend/src/utils.pl
@@ -1,9 +1,19 @@
-:- module(utils, [replace/4, vector/6, is_diagonal_vector/2]).
+:- module(utils, [
+        replace/4,
+        replace_nth/4, 
+        vector/6,
+        is_diagonal_vector/2
+    ]).
 
 % replace(+Element, +List, +NewElement, -NewList)
 replace(Element, List, NewElement, NewList) :-
     append(Start, [Element | End], List),
     append(Start, [NewElement | End], NewList), !.
+
+% replace_nth(+Index, +List, +NewElement, -NewList).
+replace_nth(Index, List, NewElement, NewList) :-
+    nth0(Index, List, _, Rest),
+    nth0(Index, NewList, NewElement, Rest).
 
 % vector(+XStart, +XEnd, +YStart, +YEnd, -X, -Y).
 vector(XStart, XEnd, YStart, YEnd, X, Y) :-
