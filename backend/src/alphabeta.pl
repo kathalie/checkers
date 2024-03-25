87aee057f02fbd53300d2205c8f8ed0281cf9eef
Kathryn Verkhohliad <verkhogliadkate@gmail.com>
2024-03-25 18:46:53 +0100

    implemented minimax

--- backend/src/alphabeta.pl
+++ backend/src/alphabeta.pl
@@ -1,143 +1,137 @@
-:- module(alphabeta, [
-    alphabeta/4
+:- module(minimax, [
+    minimax/4
 ]).
 
 :- use_module(mechanics).
 :- use_module(evaluation).
 
-next_player(white, black).
-next_player(black, white).
+/**
+% board_eating, white, 1
+
+boards:board_eating(Board), 
+write("Board:"), nl, boards:print_board(Board), nl,
+minimax(Board, 1, white, NewBoard), 
+write("New Board: "), nl, boards:print_board(NewBoard), nl, 
+!.
+
+
+% board_eating, black, 1
+
+boards:board_eating(Board), 
+write("Board:"), nl, boards:print_board(Board), nl,
+minimax(Board, 1, black, NewBoard), 
+write("New Board: "), nl, boards:print_board(NewBoard), nl, 
+!.
+
+
+% board_eating, black, 3
+
+boards:board_eating(Board), 
+write("Board:"), nl, boards:print_board(Board), nl,
+minimax(Board, 1, black, NewBoard), 
+write("New Board: "), nl, boards:print_board(NewBoard), nl, 
+!.
+
+
+
+% board_cannot_eat, black, 3
+
+boards:board_cannot_eat(Board), 
+write("Board:"), nl, boards:print_board(Board), nl,
+minimax(Board, 1, black, NewBoard), 
+write("New Board: "), nl, boards:print_board(NewBoard), nl, 
+!.
+
+% board_cannot_eat, white, 3
+
+boards:board_cannot_eat(Board), 
+write("Board:"), nl, boards:print_board(Board), nl,
+minimax(Board, 1, white, NewBoard), 
+write("New Board: "), nl, boards:print_board(NewBoard), nl, 
+!.
+*/
+
+
+
+
+% function minimax(position, depth, alpha, beta, maximizing)
+% 	if depth == 0 or game over in position
+% 		return static evaluation of position
+% 	if maximizing
+% 		maxEval = -infinity
+% 		for each child of position
+% 			eval = minimax(child, depth - 1, alpha, beta false)
+% 			maxEval = max(maxEval, eval)
+% 			alpha = max(alpha, eval)
+% 			if beta <= alpha
+% 				break
+% 		return maxEval
+% 	else
+% 		minEval = +infinity
+% 		for each child of position
+% 			eval = minimax(child, depth - 1, alpha, beta true)
+% 			minEval = min(minEval, eval)
+% 			beta = min(beta, eval)
+% 			if beta <= alpha
+% 				break
+% 		return minEval
+
 
 maximizing(white).
 minimizing(black).
 
-% function minimax(position, depth, alpha, beta, maximizingPlayer)
-% 	if depth == 0 or game over in position
-% 		return static evaluation of position
+% minimax(+Board, +Depth, +Player, -BestBoard).
+minimax(Board, Depth, Player, BestBoard) :- 
+    minimax(Board, Depth, -9999, 9999, Player, Eval, BestBoard), write(Eval), nl.
 
-% minimax(Board, Depth, _, _, _, Eval) :-  
-%     (
-%         game_over(Board); 
-%         Depth =:= 0
-%     ),
-%     evaluate_board(Board, Eval).
+% minimax(+Board, +Depth, +Alpha, +Beta, +Player, -Eval, -BestBoard).
+% Base cases when the game is over or it is the maximum wanted depth.
+minimax(Board, Depth, _, _, Player, Eval, Board) :-  
+    (
+        game_over(Board);
+        Depth =:= 0
+    ),
+    evaluate_board(Player, Board, Eval).
+    % boards:print_board(Board), nl, write("Eval: "), write(Eval), nl.
 
-% minimax(Board, Depth, Alpha, Beta, Player, Eval) :- 
-%     minimax_helper(Board, NewBoards, Depth, NewDepth, Player, NextPlayer),
-%     minimizing(Player),
-%     minEval(NewBoards, NewDepth, Alpha, Beta, NextPlayer, 9999, Eval).
+% Case when to get the worst board for the opponent.
+minimax(Board, Depth, Alpha, Beta, Player, Eval, BestBoard) :- 
+    minimax_helper(Board, NewBoard, Depth, NewDepth, Player),
+    minimizing(Player),
+    minEval(NewBoard, NewDepth, Alpha, Beta, Player, 9999, Eval, BestBoard).
 
-% minimax(Board, Depth, Alpha, Beta, Player, Eval) :- 
-%     minimax_helper(Board, NewBoards, Depth, NewDepth, Player, NextPlayer),
-%     maximizing(Player),
-%     maxEval(NewBoards, NewDepth, Alpha, Beta, NextPlayer, -9999, Eval).
-
-% minimax_helper(Board, NewBoards, Depth, NewDepth, Player, NextPlayer) :-
-%     Depth > 0,
-%     \+ game_over(Board),
-%     possible_actions(Player, Board, NewBoards),
-%     next_player(Player, NextPlayer),
-%     NewDepth is Depth - 1.
-
-% % 	if maximizingPlayer
-% % 		maxEval = -infinity
-% % 		for each child of position
-% % 			eval = minimax(child, depth - 1, alpha, beta false)
-% % 			maxEval = max(maxEval, eval)
-% % 			alpha = max(alpha, eval)
-% % 			if beta <= alpha
-% % 				break
-% % 		return maxEval
-% maxEval([], _, _, _, _, MaxEval, MaxEval).
-
-% maxEval([NewBoard | Rest], Depth, Alpha, Beta, NextPlayer, TempMaxEval, MaxEval) :-
-%     minimax(NewBoard, Depth, Alpha, Beta, NextPlayer, Eval),
-%     NewMaxEval is max(TempMaxEval, Eval),
-%     NewAlpha is max(Alpha, Eval),
-%     (
-%         Beta =< NewAlpha, % If Beta is less than or equal to NewAlpha
-%         MaxEval = NewMaxEval % Set MaxEval to NewMaxEval
-%     ;
-%         Beta > NewAlpha, % Otherwise
-%         maxEval(Rest, Depth, NewAlpha, Beta, NextPlayer, NewMaxEval, MaxEval) % Continue with recursion
-%     ).
-
-% % 	else
-% % 		minEval = +infinity
-% % 		for each child of position
-% % 			eval = minimax(child, depth - 1, alpha, beta true)
-% % 			minEval = min(minEval, eval)
-% % 			beta = min(beta, eval)
-% % 			if beta <= alpha
-% % 				break
-% % 		return minEval
-% minEval([], _, _, _, _, MinEval, MinEval).
-
-% minEval([NewBoard | Rest], Depth, Alpha, Beta, NextPlayer, TempMinEval, MinEval) :-
-%     minimax(NewBoard, Depth, Alpha, Beta, NextPlayer, Eval),
-%     NewMinEval is min(TempMinEval, Eval),
-%     NewBeta is min(Beta, Eval),
-%     (
-%         NewBeta =< Alpha, % If Beta is less than or equal to Alpha
-%         MinEval = NewMinEval % Set MinEval to NewMinEval
-%     ;
-%         NewBeta > Alpha, % Otherwise
-%         minEval(Rest, Depth, Alpha, NewBeta, NextPlayer, NewMinEval, MinEval) % Continue with recursion
-%     ).
+% Case when to get the best board for the current player.
+minimax(Board, Depth, Alpha, Beta, Player, Eval, BestBoard) :- 
+    minimax_helper(Board, NewBoard, Depth, NewDepth, Player),
+    maximizing(Player),
+    maxEval(NewBoard, NewDepth, Alpha, Beta, Player, -9999, Eval, BestBoard).
 
 
-
-% best_action(Depth, Board, BestBoard) :-
-%     findall(
-%         NewBoard-Eval, 
-%         (
-%             minimax(Board, Depth, -1000, 1000, white, Eval), 
-%             NewBoard
-%         ), 
-%         Boards
-%     ),
-%     max_member(BestBoard-Eval, Boards).
+% minimax_helper(+Board, -NewBoard, +Depth, -NewDepth, +Player).
+minimax_helper(Board, NewBoard, Depth, NewDepth, Player) :-
+    Depth > 0,
+    possible_actions(Player, Board, NewBoard),
+    NewDepth is Depth - 1.
 
 
-alphabeta(Player, Board, Depth, BestBoard) :- 
-    alphabeta(Player, Board, Depth, -1000, 1000, BestBoard, _).
+% maxEval(+Board, +Depth, +Alpha, +Beta, +Player, +TempMaxEval, -MaxEval, -Board).
+maxEval(Board, Depth, Alpha, Beta, Player, TempMaxEval, MaxEval, Board) :-
+    opponent(Player, NextPlayer),
+    minimax(Board, Depth, Alpha, Beta, NextPlayer, Eval, _),
+    MaxEval is max(TempMaxEval, Eval),
+    NewAlpha is max(Alpha, Eval),
+    (
+        Beta > NewAlpha;
+        Beta =< NewAlpha, !
+    ).
 
-alphabeta(Player, Board, Depth, Alpha, Beta, GoodBoard, Eval) :-
-    Depth > 0, \+game_over(Board),
-    possible_actions(Player, Board, NewBoards), !,
-    boundedbest(Player, NewBoards, Depth, Alpha, Beta, GoodBoard, Eval);
-    evaluate_board(Board, Eval).        % Static value of Board
-
-boundedbest(Player, [Board|OtherBoards], Depth, Alpha, Beta, GoodBoard, GoodEval) :-
-    NewDepth is Depth - 1,
-    alphabeta(Player, Board, NewDepth, Alpha, Beta, _, Eval),
-    goodenough(Player, OtherBoards, Depth, Alpha, Beta, Board, Eval, GoodBoard, GoodEval).
-
-goodenough(_, [], _, _, _, Board, Eval, Board, Eval) :- !.     % No other candidate
-
-goodenough(Player, _, _, Alpha, Beta, Board, Eval, Board, Eval) :-
-    minimizing(Player), Eval > Beta, !;       % Maximizer attained upper bound
-    maximizing(Player), Eval < Alpha, !.      % Minimizer attained lower bound
-
-goodenough(Player, NewBoards, Depth, Alpha, Beta, Board, Eval, GoodBoard, GoodEval) :-
-    newbounds(Player, Alpha, Beta, Eval, NewAlpha, NewBeta),        % Refine bounds
-    boundedbest(Player, NewBoards, Depth, NewAlpha, NewBeta, GoodBoard, GoodEval),
-    betterof(Player, Board, Eval, GoodBoard, GoodEval, GoodBoard, GoodEval).
-
-newbounds(Player, Alpha, Beta, Eval, Eval, Beta) :-
-    minimizing(Player), Eval > Alpha, !.        % Maximizer increased lower bound
-
-newbounds(Player, Alpha, Beta, Eval, Alpha, Eval) :-
-    maximizing(Player), Eval < Beta, !.         % Minimizer decreased upper bound
-
-newbounds(_, Alpha, Beta, _, Alpha, Beta).          % Otherwise bounds unchanged
-
-betterof(Player, Board, Eval, _, GoodEval, Board, Eval) :-         % Board better then GoodBoard
-    minimizing(Player), Eval > GoodEval, !;
-    maximizing(Player), Eval < GoodEval, !.
-
-betterof(_, _, _, GoodBoard, GoodEval, GoodBoard, GoodEval).             % Otherwise GoodBoard better
-
-
-
-%TODO minimax!!!!
+% minEval(+Board, +Depth, +Alpha, +Beta, +Player, +TempMaxEval, -MinEval, -Board).
+minEval(Board, Depth, Alpha, Beta, Player, TempMinEval, MinEval, Board) :-
+    opponent(Player, NextPlayer),
+    minimax(Board, Depth, Alpha, Beta, NextPlayer, Eval, _),
+    MinEval is min(TempMinEval, Eval),
+    NewBeta is min(Beta, Eval),
+    (
+        NewBeta > Alpha;
+        NewBeta =< Alpha, !
+    ).
