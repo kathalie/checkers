97821f09dd2048d8784d5ecc7bfea41120fecaaf
Dmytro Popov <thedmitryp@ukr.net>
2024-03-25 18:11:53 +0000

    Renamed json fields in `decodeMovement`

--- checkers_front/lib/src/http/movement_from_json.dart
+++ checkers_front/lib/src/http/movement_from_json.dart
@@ -1,8 +1,8 @@
 import '../domain/typedefs.dart';
 
 Movement decodeMovement(Map<String, dynamic> json) {
-  final from = (json['xFrom'] as int, json['yFrom'] as int);
-  final to = (json['xTo'] as int, json['yTo'] as int);
+  final from = (json['rFrom'] as int, json['cFrom'] as int);
+  final to = (json['rTo'] as int, json['cTo'] as int);
 
   return (from: from, to: to);
 }
