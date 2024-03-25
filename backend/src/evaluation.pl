0f441c29f234af1057b126e66c34a60907e5386f
Kathryn Verkhohliad <verkhogliadkate@gmail.com>
2024-03-25 14:44:25 +0100

    fixed evaluation and `possible_actions `

--- backend/src/evaluation.pl
+++ backend/src/evaluation.pl
@@ -1,42 +1,46 @@
 :- module(evaluation, [
-    evaluate_board/2,
-    wins/2
-    ]).
+    evaluate_board/3
+]).
 
-% Checks if there is a winner.
-% wins(+Board, ?Ch).
-wins(Board, b) :-
-    \+ member(cell(w, _, _), Board),
-    \+ member(cell(wq, _, _), Board).
+:- use_module(mechanics).
 
-% wins(+Board, ?Ch).
-wins(Board, w) :-
-    \+ member(cell(b, _, _), Board),
-    \+ member(cell(bq, _, _), Board).
+static_material_value(w, 1).
+static_material_value(b, 1).
+static_material_value(wq, 2).
+static_material_value(bq, 2).
 
-material_value(b, 1).
-material_value(w, -1).
-material_value(bq, 2).
-material_value(wq, -2).
+% Evaluates Ch for a Player.
+% Example: for a "white" Player "b" will result in -1, "wq" in 2.
+% material_value(+Player, +Ch, -Eval).
+material_value(Player, Ch, Eval) :-
+    player(Player, Ch),
+    static_material_value(Ch, Eval), !.
 
-% evaluate_board(+Board, -Score)
-evaluate_board(Board, -1000) :-
-    wins(Board, b).
+material_value(Player, Ch, Eval) :-
+    \+ player(Player, Ch),
+    static_material_value(Ch, AbsEval),
+    Eval is -AbsEval, !.
 
-evaluate_board(Board, 1000) :-
-    wins(Board, w).
+% evaluate_board(+Player, +Board, -Score).
+evaluate_board(Player, Board, 1000) :- 
+    wins(Board, Player).
 
-evaluate_board(Board, Score) :-
-    evaluate_board(Board, 0, Score).
+evaluate_board(Player, Board, -1000) :- 
+    player(Player, Opponent),
+    wins(Board, Opponent).
 
-evaluate_board([], Score, Score).
+evaluate_board(Player, Board, Score) :-
+    evaluate_board(Player, Board, 0, Score).
 
-evaluate_board([cell(Ch, Row, Col) | RestOfBoard], CurrentScore, Score) :-
-    material_value(Ch, AddToScore),
+% evaluate_board(+Player, +Board, +PrevScore, -Score).
+evaluate_board(_, [], Score, Score) :- !.
+
+evaluate_board(Player, [cell(Ch, Row, Col) | RestOfBoard], CurrentScore, Score) :-
+    material_value(Player, Ch, AddToScore),
     NewScore is CurrentScore + AddToScore,
-    evaluate_board(RestOfBoard, NewScore, Score).
+    evaluate_board(Player, RestOfBoard, NewScore, Score).
 
 
-% % positional_value(+Ch, +Row, -Value)
+% % positional_value(+Ch, +Row, -Value).
 % positional_value(Ch, Row, Value) :-
 %     Coef = 
