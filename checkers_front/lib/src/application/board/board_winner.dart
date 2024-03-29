5aec6e7d86f0a736432ec30f98197b84d5688739
Dmytro Popov <thedmitryp@ukr.net>
2024-03-29 00:25:50 +0000

    Fixed the game over state not being detected
    When no checkers of the player can move

--- checkers_front/lib/src/application/board/board_winner.dart
+++ checkers_front/lib/src/application/board/board_winner.dart
@@ -3,14 +3,20 @@ import 'board.dart';
 
 extension BoardWinner on Board {
   CheckerColor? get winner {
-    if (whites.isEmpty) {
+    if (whites.isEmpty || !canMove(CheckerColor.white)) {
       return CheckerColor.black;
     }
 
-    if (blacks.isEmpty) {
+    if (blacks.isEmpty || !canMove(CheckerColor.black)) {
       return CheckerColor.white;
     }
 
     return null;
+  }
+
+  bool canMove(CheckerColor player) {
+    final checkers = player == CheckerColor.white ? whites : blacks;
+
+    return checkers.expand((pos) => possibleMoves(from: pos)).isNotEmpty;
   }
 }
